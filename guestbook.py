6d3c6920452e1d109a30bd139ed7f61b702bcd9e
Eduard Stanciu <Eduard Stanciu>
2016-07-03 19:07:31 +0100

    Initial Commit

--- guestbook.py
+++ guestbook.py
@@ -20,6 +20,7 @@ import urllib
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
+import hashlib, uuid
 
 import jinja2
 import webapp2
@@ -30,61 +31,44 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 # [END imports]
 
-DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
-
-
-# We set a parent key on the 'Greetings' to ensure that they are all
-# in the same entity group. Queries across the single entity group
-# will be consistent. However, the write rate should be limited to
-# ~1/second.
-
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    """Constructs a Datastore key for a Guestbook entity.
-
-    We use guestbook_name as the key.
-    """
-    return ndb.Key('Guestbook', guestbook_name)
-
-
-# [START greeting]
-class Author(ndb.Model):
-    """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
-
-
-class Greeting(ndb.Model):
+# [START signature]
+class Signature(ndb.Model):
     """A main model for representing an individual Guestbook entry."""
-    author = ndb.StructuredProperty(Author)
-    content = ndb.StringProperty(indexed=False)
+    first_name = ndb.StringProperty(indexed=False)
+    last_name = ndb.StringProperty(indexed=False)
+    dob = ndb.StringProperty(indexed=False)
+    email = ndb.StringProperty(indexed=False)
+    pwd = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
-# [END greeting]
+# [END signature]
 
 
 # [START main_page]
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
-
-        user = users.get_current_user()
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
+        guestbook_first_name = self.request.get('guestbook_first_name')
+        guestbook_last_name = self.request.get('guestbook_last_name')
+        guestbook_dob = self.request.get('guestbook_dob')
+        guestbook_email = self.request.get('guestbook_email')
+        guestbook_pwd = self.request.get('guestbook_pwd')
+        signatures_array = []
+        signatures =  Signature.query().fetch(20,keys_only=True)
+        for signature in signatures:
+            signature_object = signature.get()
+            temp_signature = Signature(first_name = signature_object.first_name,
+                                     last_name = signature_object.last_name,
+                                     dob = signature_object.dob,
+                                     email = signature_object.email,
+                                     pwd = signature_object.pwd)
+            signatures_array.append(temp_signature)
         template_values = {
-            'user': user,
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
+            'signatures': signatures_array,
+            'guestbook_first_name': urllib.quote_plus(guestbook_first_name),
+            'guestbook_last_name': urllib.quote_plus(guestbook_last_name),
+            'guestbook_dob': urllib.quote_plus(guestbook_dob),
+            'guestbook_email': urllib.quote_plus(guestbook_email),
+            'guestbook_pwd': urllib.quote_plus(guestbook_pwd),
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -96,25 +80,32 @@ class MainPage(webapp2.RequestHandler):
 class Guestbook(webapp2.RequestHandler):
 
     def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each
-        # Greeting is in the same entity group. Queries across the
+        # We set the same parent key on the 'signature' to ensure each
+        # signature is in the same entity group. Queries across the
         # single entity group will be consistent. However, the write
         # rate to a single entity group should be limited to
         # ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
+        guestbook_first_name = self.request.get('guestbook_first_name')
+        guestbook_last_name = self.request.get('guestbook_last_name')
+        guestbook_dob = self.request.get('guestbook_dob')
+        guestbook_email = self.request.get('guestbook_email')
+        guestbook_pwd = self.request.get('guestbook_pwd')
 
-        if users.get_current_user():
-            greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
-
-        greeting.content = self.request.get('content')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
+        query_params = {'guestbook_first_name': guestbook_first_name,
+                        'guestbook_last_name': guestbook_last_name,
+                        'guestbook_dob': guestbook_dob,
+                        'guestbook_email': guestbook_email,
+                        'guestbook_pwd': guestbook_pwd
+                        }
+        salt = uuid.uuid4().hex
+        hashed_password = hashlib.sha512(guestbook_pwd + salt).hexdigest()
+        new_entry = Signature(first_name=guestbook_first_name,
+                             last_name=guestbook_last_name,
+                             dob=guestbook_dob,
+                             email=guestbook_email,
+                             pwd=hashed_password)
+        new_entry.put()
+        self.redirect('/')
 # [END guestbook]
 
 
